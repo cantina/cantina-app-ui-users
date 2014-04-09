85b0bbdc541cfa66c1022d98fee01212507ef04e
erinlesavoy <erin.lesavoy@gmail.com>
2014-04-09 14:52:21 -0400

    Fixes app.load for plugins

--- cantina-app-users-ui.js
+++ cantina-app-users-ui.js
@@ -1,7 +1,7 @@
 var app = require('cantina');
 
 require('cantina-app-users');
-require('./plugins/handlebars');
+app.load('plugins', __dirname);
 app.loadMiddleware('middleware', __dirname);
 app.loadControllers('controllers', __dirname);
 app.loadViews('views', __dirname);
