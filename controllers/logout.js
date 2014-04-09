4cb545f54548bf1d95bbe55b4e2c686fda8286f5
erinlesavoy <erin.lesavoy@gmail.com>
2014-04-09 12:28:04 -0400

    Fixes logout controller

--- controllers/logout.js
+++ controllers/logout.js
@@ -4,6 +4,7 @@ var app = require('cantina')
 var sessionConf = app.conf.get('session') || {}
   , resetCookie = sessionConf.cookie ? require('util').format('%s=null; path=%s; expires=Thu, 01 Jan 1970 00:00:00 GMT%s', sessionConf.key, sessionConf.cookie.path, (sessionConf.cookie.httpOnly ? '; HttpOnly' : '')) : undefined;
 
+app.middleware.remove('/logout');
 controller.get('/logout', function (req, res, next) {
   app.auth.logOut(req, function (err) {
     if (err) return res.renderError(err);
