c30e6e7196ecd5522f8e7e777bc07dc6ddf98e67
Dan MacTough <danmactough@gmail.com>
2014-04-16 21:36:14 -0400

    Revert "Don't re-require cantina-app-users"
    This reverts commit 91f760400296678b971ee21bc1f5c7abc054dde9.

--- cantina-app-ui-users.js
+++ cantina-app-ui-users.js
@@ -1,6 +1,6 @@
 var app = require('cantina');
 
-if (!app.users) require('cantina-app-users');
+require('cantina-app-users');
 app.load('plugins', __dirname);
 app.loadMiddleware('middleware', __dirname);
 app.loadControllers('controllers', __dirname);
